b7d85b83ed03fb6277dc1ac606aa6e2ad6fca798
Shehzad <abmshehzad@gmail.com>
2022-03-27 16:50:17 +0500

    Walls page update

--- resources/views/walls.blade.php
+++ resources/views/walls.blade.php
@@ -3,55 +3,61 @@
 @section('content')
     <div class="dashboard gallery mini">
         @for($i = 1; $i < 4; $i++)
-            <h4 class="font-secondary">
+            <h4 class="font-secondary section__title">
                 Version {{ $i }}
             </h4>
-            <div class="inner__field">
-                <div class="card overflow-hidden">
-                    <div class="card-body">
-                        <img
-                            src="{{ asset('images/card-img.png') }}"
-                            alt="image"
-                            class="bg__image"
-                        />
-                        <a href="{{ route('editor') }}" class="add__btn">
-                            <span class="plus__icon">
-                                <x-svg.plus width="35" height="35"/>
-                            </span>
-                            Add New Option
-                        </a>
-                    </div>
-                </div>
-                @for($j = 1; $j < 6; $j++)
-                    <div class="card">
-                        <div class="card-img-top">
+            <div class="inner__field row mb-4">
+                <div class="col-md-6 col-lg-3 mb-3 ">
+                    <div href="#" class="card overflow-hidden">
+                        <a href="#" class="overlay__link"></a>
+                        <div class="card-body">
                             <img
                                 src="{{ asset('images/card-img.png') }}"
                                 alt="image"
-                                width="100%"
-                                height="auto"
+                                class="bg__image"
                             />
+                            <a href="{{ route('editor') }}" class="add__btn">
+                                <span class="plus__icon">
+                                    <x-svg.plus width="35" height="35"/>
+                                </span>
+                                Add New Option
+                            </a>
                         </div>
-                        <div class="card-body">
-                            <div class="accordion__item">
-                                <div class="accordion__header">
-                                    <div class="left">
-                                        <div class="user__details">
-                                            <h3 class="username">Matis Centered</h3>
-                                            <div class="tag">Nile Berry | 12/11/2021</div>
-                                            <div class="profiles__icons">
-                                                @include('include.partials.contributors')
+                    </div>
+                </div>
+                @for($j = 1; $j < 6; $j++)
+                    <div class="col-md-6 col-lg-3 mb-3 ">
+                        <div href="#" class="card">
+                            <a href="#" class="overlay__link"></a>
+                            <div class="card-img-top">
+                                <img
+                                    src="{{ asset('images/card-img.png') }}"
+                                    alt="image"
+                                    width="100%"
+                                    height="auto"
+                                />
+                            </div>
+                            <div class="card-body">
+                                <div class="accordion__item">
+                                    <div class="accordion__header">
+                                        <div class="left">
+                                            <div class="user__details">
+                                                <h3 class="username">Matis Centered</h3>
+                                                <div class="tag">Nile Berry | 12/11/2021</div>
+                                                <div class="profiles__icons">
+                                                    @include('include.partials.contributors')
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="right">
-                                        <div class="surface__items">
-                                            <a href="#" class="icon">
-                                                <x-svg.trash-can/>
-                                            </a>
-                                            <a href="#" class="icon">
-                                                <x-svg.thumbs-up/>
-                                            </a>
+                                        <div class="right">
+                                            <div class="surface__items">
+                                                <a href="#" class="icon">
+                                                    <x-svg.trash-can/>
+                                                </a>
+                                                <a href="#" class="icon">
+                                                    <x-svg.thumbs-up/>
+                                                </a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
