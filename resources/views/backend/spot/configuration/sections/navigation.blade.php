402f2f917bc649a43b1e3b3062751e20d6134655
Abdul Majeed Shehzad <abmshehzad@gmail.com>
2022-06-22 01:47:52 +0500

    Update navigation.blade.php

--- resources/views/backend/spot/configuration/sections/navigation.blade.php
+++ resources/views/backend/spot/configuration/sections/navigation.blade.php
@@ -3,20 +3,28 @@
         <h5>{{ __('Spot ') . $spot->id }}</h5>
         <div class="row mt-3">
             <x-backend::inputs.input
+                col="col" name="navigations[{{$spot->id}}][rx]" label="rx"
+                :value="$spot->xml->navigations[$spot->id]['rx'] ?? ''"
+            />
+            <x-backend::inputs.input
+                col="col" name="navigations[{{$spot->id}}][hlookat]" label="hlookat"
+                :value="$spot->xml->navigations[$spot->id]['hlookat'] ?? ''"
+            />
+            <x-backend::inputs.input
+                col="col" name="navigations[{{$spot->id}}][vlookat]" label="vlookat"
+                :value="$spot->xml->navigations[$spot->id]['vlookat'] ?? ''"
+            />
+            <x-backend::inputs.input
                 col="col" name="navigations[{{$spot->id}}][ath]" label="ath"
-                :value="$spot->xml->navigations[$spot->id]['background']['ath'] ?? ''"
+                :value="$spot->xml->navigations[$spot->id]['ath'] ?? ''"
             />
             <x-backend::inputs.input
                 col="col" name="navigations[{{$spot->id}}][atv]" label="atv"
-                :value="$spot->xml->navigations[$spot->id]['background']['atv'] ?? ''"
+                :value="$spot->xml->navigations[$spot->id]['atv'] ?? ''"
             />
             <x-backend::inputs.input
                 col="col" name="navigations[{{$spot->id}}][scale]" label="scale"
-                :value="$spot->xml->navigations[$spot->id]['background']['scale'] ?? ''"
-            />
-            <x-backend::inputs.input
-                col="col" name="navigations[{{$spot->id}}][zorder]" label="zorder"
-                :value="$spot->xml->surfaces[$spot->id]['background']['zorder'] ?? ''"
+                :value="$spot->xml->navigations[$spot->id]['scale'] ?? ''"
             />
         </div>
     </div>
