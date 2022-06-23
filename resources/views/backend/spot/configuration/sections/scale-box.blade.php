866e1059e68efc1f65c9593d3b82dec08bf8ab93
Abdul Majeed Shehzad <abmshehzad@gmail.com>
2022-06-23 19:59:47 +0500

    Update scale-box.blade.php

--- resources/views/backend/spot/configuration/sections/scale-box.blade.php
+++ resources/views/backend/spot/configuration/sections/scale-box.blade.php
@@ -4,20 +4,20 @@
             <h5>
                 <span class="me-2">{{ $name }}</span>
                 <x-backend::inputs.switch
-                    col="col-12 mb-3" name="scale_box[square][enabled]" label=""
+                    col="col-12 mb-3" name="scale_box[{{$scale}}][enabled]" label=""
                     checked="{{ $spot->xml->scale_box[$scale]['enabled'] ?? false }}"
                 />
             </h5>
             <x-backend::inputs.input
-                col="col" name="scale_box[square][ath]" label="ath"
+                col="col" name="scale_box[{{$scale}}][ath]" label="ath"
                 :value="$spot->xml->scale_box[$scale]['ath'] ?? 0"
             />
             <x-backend::inputs.input
-                col="col" name="scale_box[square][atv]" label="atv"
+                col="col" name="scale_box[{{$scale}}][atv]" label="atv"
                 :value="$spot->xml->scale_box[$scale]['atv'] ?? 0"
             />
             <x-backend::inputs.input
-                col="col" name="scale_box[square][scale]" label="scale"
+                col="col" name="scale_box[{{$scale}}][scale]" label="scale"
                 :value="$spot->xml->scale_box[$scale]['scale'] ?? '0.7'"
             />
         </div>
