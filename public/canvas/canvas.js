e095db9953ea8351b123bd87bf861db1f4217b26
Abdul Majeed Shehzad <abmshehzad@gmail.com>
2022-09-29 12:46:54 +0500

    Update canvas.js

--- public/canvas/canvas.js
+++ public/canvas/canvas.js
@@ -1,7 +1,7 @@
 import * as canvas from './artwork_assignment.js';
 
 $(function (){
-    $('.artwork-img').click(function (){
+    $('#site__body').on('click', '.artwork-img',function (){
         let target = $(this).get(0);
         let newSelection = canvas.newArtworkSelection(target);
         canvas.placeSelectedImage(newSelection);
