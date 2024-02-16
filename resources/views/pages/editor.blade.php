2c6a2fb69357ff730251637b5cb189df219d7ff9
Abdul Majeed Shehzad <abmshehzad@gmail.com>
2024-02-16 10:07:04 +0500

    Update editor.blade.php

--- resources/views/pages/editor.blade.php
+++ resources/views/pages/editor.blade.php
@@ -55,9 +55,11 @@
                 <livewire:artwork-collection :project="$project"/>
 
                 @php($canvasId = $selectedSurfaceState ? $selectedSurfaceState->id : 'new')
-                <div class="col-9 main-col position-relative" x-data="{ changedCanvases: 0, activeCanvas: @js("artwork_canvas_$canvasId") }">
+                <div class="col-9 main-col position-relative"
+                     x-data="{ changedCanvases: 0, activeCanvas: @js("artwork_canvas_$canvasId") }">
                     <x-editor-actions/>
-                    <div style="position: absolute; inset: auto 5px 0 auto; z-index: 10" class="alert alert-warning" x-show="changedCanvases > 1">
+                    <div style="position: absolute; inset: auto 5px 0 auto; z-index: 10" class="alert alert-warning"
+                         x-show="changedCanvases > 1">
                         <i class="fal fa-exclamation-triangle"></i> Multiple canvases have unsaved changes. <br>
                         Updating one canvas at a time will discard changes on others.
                     </div>
@@ -83,7 +85,8 @@
                                  @click="activeCanvas = @js($canvas['canvasId']); $dispatch('canvasChanged', { surfaceStateId: @js($canvas['surfaceStateId']) })">
                                 <div>
                                     <span>
-                                        <i x-show="hasChanges" x-ref="unsavedChanges" class="fa fa-circle fa-xs text-warning change-icon"></i>
+                                        <i x-show="hasChanges" x-ref="unsavedChanges"
+                                           class="fa fa-circle fa-xs text-warning change-icon"></i>
                                         <span class="surface-name">{{ $canvas['surfaceStateName'] }}</span>
                                     </span>
                                     @if($canvas['surfaceStateId'] && $canvas['surfaceStateId'] !== $currentSurfaceStateId)
@@ -97,14 +100,22 @@
                                               action="{{ route('surfaces.destroy', $canvas['surfaceStateId']) }}">
                                             @method('delete')
                                             @csrf
-                                            <button class="cross-btn" onclick="return confirm('Are you sure you want to delete this version?');"
+                                            <button class="cross-btn"
+                                                    onclick="return confirm('Are you sure you want to delete this version?');"
                                                     style="line-height: 0">
-{{--                                                <i class="fas fa-trash"></i>--}}
-                                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M1.875 3.75H3.125H13.125" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                                    <path d="M5 3.75V2.5C5 2.16848 5.1317 1.85054 5.36612 1.61612C5.60054 1.3817 5.91848 1.25 6.25 1.25H8.75C9.08152 1.25 9.39946 1.3817 9.63388 1.61612C9.8683 1.85054 10 2.16848 10 2.5V3.75M11.875 3.75V12.5C11.875 12.8315 11.7433 13.1495 11.5089 13.3839C11.2745 13.6183 10.9565 13.75 10.625 13.75H4.375C4.04348 13.75 3.72554 13.6183 3.49112 13.3839C3.2567 13.1495 3.125 12.8315 3.125 12.5V3.75H11.875Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                                    <path d="M6.25 6.875V10.625" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                                    <path d="M8.75 6.875V10.625" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                {{--                                                <i class="fas fa-trash"></i>--}}
+                                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none"
+                                                     xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M1.875 3.75H3.125H13.125" stroke="black" stroke-width="1.5"
+                                                          stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path
+                                                        d="M5 3.75V2.5C5 2.16848 5.1317 1.85054 5.36612 1.61612C5.60054 1.3817 5.91848 1.25 6.25 1.25H8.75C9.08152 1.25 9.39946 1.3817 9.63388 1.61612C9.8683 1.85054 10 2.16848 10 2.5V3.75M11.875 3.75V12.5C11.875 12.8315 11.7433 13.1495 11.5089 13.3839C11.2745 13.6183 10.9565 13.75 10.625 13.75H4.375C4.04348 13.75 3.72554 13.6183 3.49112 13.3839C3.2567 13.1495 3.125 12.8315 3.125 12.5V3.75H11.875Z"
+                                                        stroke="black" stroke-width="1.5" stroke-linecap="round"
+                                                        stroke-linejoin="round"/>
+                                                    <path d="M6.25 6.875V10.625" stroke="black" stroke-width="1.5"
+                                                          stroke-linecap="round" stroke-linejoin="round"/>
+                                                    <path d="M8.75 6.875V10.625" stroke="black" stroke-width="1.5"
+                                                          stroke-linecap="round" stroke-linejoin="round"/>
                                                 </svg>
 
                                             </button>
@@ -121,10 +132,12 @@
                                 <i class="fas fa-plus btn"></i>
                             </a>
                         @endif
-                            <x-menu-item
-                                text="Return to 360 View" class="view-360" :img="asset('redesign/images/360-icon.png')" target="_self"
-                                :route="route('tours.show', array_merge(request()->all(), ['tour' => $tour]))"
-                            />
+
+                        <x-menu-item
+                            text="Return to 360 View" class="view-360" :img="asset('redesign/images/360-icon.png')"
+                            target="_self"
+                            :route="route('tours.show', array_merge(request()->all(), ['tour' => $tour]))"
+                        />
 
                     </div>
 
