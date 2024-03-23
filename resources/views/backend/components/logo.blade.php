2f1383db8f08154fbc7127fe0800a835ae204f81
Zia Ur Rehman <117204767+Ziaurrehman780@users.noreply.github.com>
2024-03-23 04:32:21 -0700

    remove the sidebar's logo height attribute

--- resources/views/backend/components/logo.blade.php
+++ resources/views/backend/components/logo.blade.php
@@ -6,7 +6,7 @@
 
 <a href="{{ route('dashboard') }}" class="logo text-center logo-{{ $mode }} ">
     <span class="logo-lg ">
-        <img width="150" src="{{ $logo }}" alt="" height="40">
+        <img width="150" src="{{ $logo }}" alt="">
     </span>
     <span class="logo-sm ">
         <img src="{{ $logo_small }}" alt="" height="40">
