1624aef7907feed88086817813ce0b6a4d2e761f
musordmt <musordmt@proton.me>
2024-10-22 18:53:09 +0300

    update auto save function

--- resources/views/components/editor-actions.blade.php
+++ resources/views/components/editor-actions.blade.php
@@ -1,10 +1,10 @@
 <div class="editor-actions d-flex" style="top: 92px">
-    <button class="action btn btn-sm px-2" data-bs-toggle="modal" data-bs-target="#confirmation_modal"
+    <!-- <button class="action btn btn-sm px-2" data-bs-toggle="modal" data-bs-target="#confirmation_modal"
             data-text="Save As">
         <i class="fa fal fa-folder-upload text-black"></i>
     </button>
     <a id="save_btn" class="action btn btn-sm hide px-2" href="javascript:void(0)" data-text="Save Changes"><i
-            class="fa fal fa-floppy-disk text-black"></i></a>
+            class="fa fal fa-floppy-disk text-black"></i></a> -->
     <a id="remove_btn" class="action btn btn-sm hide px-2" href="javascript:void(0)" data-text="Remove"><i
             class="fa fal fa-window-close text-black"></i></a>
 </div>
