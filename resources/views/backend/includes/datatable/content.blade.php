4f4fbd348100b9a0a0e2619f7b10e459e948595d
LuckyRyo <luckybird1115@gmail.com>
2025-02-10 04:46:19 +0900

    align the sculpture table

--- resources/views/backend/includes/datatable/content.blade.php
+++ resources/views/backend/includes/datatable/content.blade.php
@@ -1,5 +1,5 @@
 @foreach($columns as $col_name => $column)
     @if($column['visible'] && ($column['render'] ?? true))
-        <td class="td {{ $column['td-classes'] ?? '' }}" data-column="{{ $col_name }}" >{!! $row->$col_name !!}</td>
+        <td class="td text-center {{ $column['td-classes'] ?? '' }}" data-column="{{ $col_name }}" >{!! $row->$col_name !!}</td>
     @endif
 @endforeach
