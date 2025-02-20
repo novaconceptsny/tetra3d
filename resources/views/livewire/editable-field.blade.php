05b3c95809c8b2ef1279be6ec5bbd09bfb35e6a0
LuckyRyo <luckybird1115@gmail.com>
2025-02-20 23:20:55 +0900

    make the alert to confirm changes

--- resources/views/livewire/editable-field.blade.php
+++ resources/views/livewire/editable-field.blade.php
@@ -1,7 +1,7 @@
 <{{$element}} >
 <span x-data>
     @if($editing)
-        <input type="text" wire:model.live="value" x-on:click.away="$wire.updateValue()" @keydown.enter="$wire.updateValue()">
+        <input type="text" wire:model.live="value" x-on:click.away="if (confirm('All layouts of this tour will have updated surface. Proceed?')) { $wire.updateValue() }" @keydown.enter="if (confirm('All layouts of this tour will have updated surface. Proceed?')) { $wire.updateValue() }">
         @error('title') <span class="error">{{ $message }}</span> @enderror
     @else
         {{ $value }}
