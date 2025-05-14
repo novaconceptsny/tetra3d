23b280ed924e591746b0f386b23b6e2f6f2fe8a4
LuckyRyo <luckybird1115@gmail.com>
2025-05-15 01:14:11 +0900

    update the tour scale

--- resources/views/pages/tour.blade.php
+++ resources/views/pages/tour.blade.php
@@ -262,7 +262,7 @@
     var toggle_space_model = false;
     var sculpture_change_list = [];
     var surface_meshes = [];
-    var tourScale = 30.03;
+    var tourScale = 30.0;
 
     var layout_id = '{{ $layout_id }}';
     var shared_tour_id = '{{ $shared_tour_id }}';
