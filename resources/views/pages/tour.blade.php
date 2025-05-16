1dae9098057bee82d19affe2a5b7f033cd183f93
LuckyRyo <luckybird1115@gmail.com>
2025-05-16 20:22:04 +0900

    update the scale of the tour model

--- resources/views/pages/tour.blade.php
+++ resources/views/pages/tour.blade.php
@@ -262,7 +262,7 @@
     var toggle_space_model = false;
     var sculpture_change_list = [];
     var surface_meshes = [];
-    var tourScale = 30;
+    var tourScale = 30.05;
     var offsetScale = 30;
     var sculptureScale = 30;
 
