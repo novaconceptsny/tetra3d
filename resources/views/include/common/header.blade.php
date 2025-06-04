0182f4067230190ca2cda7517f221f9f36ddd49d
LuckyRyo <luckybird1115@gmail.com>
2025-06-05 00:32:38 +0900

    apply the curate 2d for only super admin

--- resources/views/include/common/header.blade.php
+++ resources/views/include/common/header.blade.php
@@ -36,12 +36,14 @@
                             <a class="nav-link" aria-current="page"
                                href="{{ route('dashboard') }}">{{ __('Tour 3D') }}</a>
                         </li>
+                        @if(auth()->user()->name === 'Super Admin')
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('tour-360.index') }}">{{ __('Tour 360') }}</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('photo.index') }}">{{ __('Photo') }}</a>
                         </li>
+                        @endif
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('inventory.index') }}">{{ __('Inventory') }}</a>
                         </li>
