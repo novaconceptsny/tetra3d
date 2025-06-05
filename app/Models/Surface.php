fc9ed771a31e76c1cda233518a119f3db3ca81ad
LuckyRyo <luckybird1115@gmail.com>
2025-06-06 02:17:44 +0900

    update the surface-tour function

--- app/Models/Surface.php
+++ app/Models/Surface.php
@@ -116,7 +116,7 @@ class Surface extends Model implements HasMedia
 
     public function tour()
     {
-        return $this->belongsTo(Tour::withoutGlobalScope('forCurrentCompany')::class);
+        return $this->belongsTo(Tour::class)->withoutGlobalScope('forCurrentCompany');
     }
 
     public function getStateThumbnail($state, $tour_is_shared = false, $has_model = 0)
