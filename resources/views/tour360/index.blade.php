e21ae208234c9766a42cb0d0a08302ac49570dbc
LuckyRyo <luckybird1115@gmail.com>
2025-06-12 23:58:48 +0900

    update the favorite layout name

--- resources/views/tour360/index.blade.php
+++ resources/views/tour360/index.blade.php
@@ -12,7 +12,7 @@
                             @foreach($favorites as $favorite)
                                 <div class="col-md-3">
                                     <div class="bg-light rounded p-3">
-                                        <h4><i class="fas fa-star text-primary"></i> {{ $favorite->project->name }}</h4>
+                                        <h4><i class="fas fa-star text-primary"></i> {{ $favorite->name }}</h4>
                                         <span>{{ $favorite->assignedTour()->name }}</span>
                                         <p class="text-end mb-0 mt-3 ">
                                             <a href="{{ route('tours.show', [$favorite->tour_id, 'layout_id' => $favorite->id]) }}" >
