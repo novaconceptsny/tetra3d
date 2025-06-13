fe08c20f2ae8f36ba3166e61c90e839cdfac045e
LuckyRyo <luckybird1115@gmail.com>
2025-06-13 23:26:45 +0900

    fix the issue on super admin

--- app/Livewire/UpdatedTourSwitcher.php
+++ app/Livewire/UpdatedTourSwitcher.php
@@ -1,16 +1,13 @@
 <?php
-
 namespace App\Livewire;
 
+use App\Models\Company;
 use App\Models\Layout;
 use App\Models\Project;
 use App\Models\Tour;
-use App\Models\Company;
 use App\Models\User;
 use WireElements\Pro\Components\SlideOver\SlideOver;
 use WireElements\Pro\Concerns\InteractsWithConfirmationModal;
-use App\Models\Sculpture;
-use App\Models\SurfaceState;
 
 class UpdatedTourSwitcher extends SlideOver
 {
@@ -23,18 +20,18 @@ class UpdatedTourSwitcher extends SlideOver
     public array $tourImages;
 
     protected $listeners = [
-        'refresh' => '$refresh',
-        'duplicateLayout' => 'duplicateLayout'
+        'refresh'         => '$refresh',
+        'duplicateLayout' => 'duplicateLayout',
     ];
 
     public function mount(Project $project)
     {
-        $this->project = $project;
-        $this->selectedTour = $project->assignedTours()->first();
+        $this->project        = $project;
+        $this->selectedTour   = $project->assignedTours()->first();
         $this->selectedTourId = $this->selectedTour?->id;
 
         $this->tourImages = $this->project->assignedTours()
-            ->mapWithKeys(function($tour) {
+            ->mapWithKeys(function ($tour) {
                 $map = $tour->maps()->first();
                 return [$tour->id => $map ? $map->getFirstMediaUrl('image') : ''];
             })
@@ -54,37 +51,35 @@ class UpdatedTourSwitcher extends SlideOver
     public static function attributes(): array
     {
         return [
-            'size' => '4xl'
+            'size' => '4xl',
         ];
     }
 
     public function toggleFavorite($layoutId)
     {
-        $layout = Layout::findOrFail($layoutId);
-        $layout->is_favorite = !$layout->is_favorite;
+        $layout              = Layout::findOrFail($layoutId);
+        $layout->is_favorite = ! $layout->is_favorite;
         $layout->save();
-        
+
         $user = auth()->user();
 
-        $company = Company::findOrFail($user->company_id);
+        if ($user && method_exists($user, 'isSuperAdmin') && $user->isSuperAdmin()) {
+            $favorites = Layout::where('is_favorite', true)
+                ->get();
+        } else {
+            $company = Company::findOrFail($user->company_id);
 
-        // Get all user IDs in this company
-        $userIds = User::where('company_id', $company->id)->pluck('id');
-
-
-        $favorites = $user && method_exists($user, 'isSuperAdmin') && $user->isSuperAdmin() 
-            ? Layout::where('is_favorite', true)
-                ->with(['tour' => function($query) {
-                    $query->withoutGlobalScope('forCurrentCompany');
-                }])
-                ->get()
-            :  Layout::where('is_favorite', true)
+            // Get all user IDs in this company
+            $userIds = User::where('company_id', $company->id)->pluck('id');
+            
+            $favorites = Layout::where('is_favorite', true)
                 ->whereIn('user_id', $userIds)
-                ->with(['tour' => function($query) {
+                ->with(['tour' => function ($query) {
                     $query->withoutGlobalScope('forCurrentCompany');
                 }])
                 ->get();
-                
+        }
+
         $this->dispatch('favoritesUpdated', favorites: $favorites);
     }
 
@@ -98,6 +93,5 @@ class UpdatedTourSwitcher extends SlideOver
             $this->dispatch('layoutDeleted', layoutId: $layout->id);
         });
     }
-
 
 }
