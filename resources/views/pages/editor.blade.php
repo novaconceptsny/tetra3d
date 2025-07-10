aba2b481fb59c3417250ad50664bdd8366d1948f
LuckyRyo <luckybird1115@gmail.com>
2025-07-11 00:15:43 +0900

    hide the list view btn

--- resources/views/pages/editor.blade.php
+++ resources/views/pages/editor.blade.php
@@ -9,7 +9,7 @@
     <x-menu-item text="Current Surface" icon="fal fa-question" :route="route('tours.surfaces', $query_params)"
         data-bs-toggle="modal" data-bs-target="#mapImage" :visible="$surface->getFirstMediaUrl('layout')" />
 
-    <x-menu-item text="List View" icon="fal fa-clone" :route="route('tours.surfaces', $query_params)" target="_self" />
+    <x-menu-item :visible="false" text="List View" icon="fal fa-clone" :route="route('tours.surfaces', $query_params)" target="_self" />
     <x-menu-item text="Map" icon="fal fa-map-marked-alt" data-bs-toggle="modal" data-bs-target="#tourMapModal" />
     <x-menu-item target="_self" text="Share" icon="fal fa-share-nodes" :visible="$layout"
         onclick="Livewire.dispatch('modal.open', {component: 'modals.share-tour', arguments: {'layout': {{ request('layout_id') }} }})" />
