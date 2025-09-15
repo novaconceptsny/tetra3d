dbe2a896c90ddc1303f9e1d6793479e661c80b46
LuckyRyo <luckybird1115@gmail.com>
2025-09-15 16:31:14 +0900

    revert the changes for copy url

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,40 +23,6 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                // If there's a redirect parameter, use it instead of the default HOME
-                if ($request->has('redirect')) {
-                    $redirectUrl = $request->get('redirect');
-                    
-                    // Validate and reconstruct URL for cross-environment compatibility
-                    if (filter_var($redirectUrl, FILTER_VALIDATE_URL)) {
-                        $parsedUrl = parse_url($redirectUrl);
-                        $currentHost = parse_url(config('app.url'), PHP_URL_HOST);
-                        
-                        // If same domain, redirect directly
-                        if ($parsedUrl['host'] === $currentHost) {
-                            return redirect($redirectUrl);
-                        }
-                        
-                        // For cross-environment, reconstruct URL
-                        if (isset($parsedUrl['path']) && !empty($parsedUrl['path'])) {
-                            $path = $parsedUrl['path'];
-                            $query = isset($parsedUrl['query']) ? '?' . $parsedUrl['query'] : '';
-                            $fragment = isset($parsedUrl['fragment']) ? '#' . $parsedUrl['fragment'] : '';
-                            $reconstructedUrl = config('app.url') . $path . $query . $fragment;
-                            
-                            if (filter_var($reconstructedUrl, FILTER_VALIDATE_URL)) {
-                                return redirect($reconstructedUrl);
-                            }
-                        }
-                    } else {
-                        // Handle relative URLs
-                        $redirectUrl = ltrim($redirectUrl, '/');
-                        if (!empty($redirectUrl)) {
-                            return redirect('/' . $redirectUrl);
-                        }
-                    }
-                }
-                
                 return redirect(RouteServiceProvider::HOME);
             }
         }
