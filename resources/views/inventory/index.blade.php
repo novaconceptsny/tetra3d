153edc41e155d6749355c73c186718ff3985e9fb
LuckyRyo <luckybird1115@gmail.com>
2025-10-13 19:01:42 +0900

    update the border color

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -470,7 +470,7 @@
                         </div>
 
                         <!-- Horizontal line -->
-                        <hr style="margin: 0px;" >
+                        <hr style="margin: 0px; background-color: #82868a;" >
                         <div style="background-color: #f5f5f5">
                             <div style="padding: 24px 24px 12px 24px;">
                                 <div class="row">
