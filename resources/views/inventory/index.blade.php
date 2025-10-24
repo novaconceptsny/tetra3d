e94f2ac5dfbd56648b7f0ddf185431a3ac52ae23
LuckyRyo <luckybird1115@gmail.com>
2025-10-24 19:55:33 +0900

    test

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -3239,11 +3239,12 @@ $(document).ready(function() {
 
     // Collection selection function
     window.selectCollection = function(collectionId, collectionName, itemCount, thumbnailUrl = null) {
-        window.selectedCollectionId = collectionId;
         console.log(collectionId, "collectionId")
         console.log(collectionName, "collectionName")
         console.log(itemCount, "itemCount")
         console.log(thumbnailUrl, "thumbnailUrl")
+        
+        window.selectedCollectionId = collectionId;
 
         // Update the dropdown button content
         const dropdownButton = document.querySelector('.dropdown button');
